45a2a115c417a53aaea65c88665583b3d94557d5
Simtel <simuls@gmail.com>
2024-07-25 09:53:27 +0400

    add logger to request

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/render"
 	"go-project/common"
 	"go-project/domains"
@@ -13,6 +14,7 @@ func main() {
 	common.InitFileStorage()
 
 	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 	r.Use(render.SetContentType(render.ContentTypeJSON))
 
 	domains.Routes(r)
