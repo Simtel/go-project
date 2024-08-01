ad196133f1e618371134c23062b932a71c224199
Simtel <simuls@gmail.com>
2024-08-01 17:18:33 +0400

    chi render

---
+++ internal/adapter/httprepo/domainsrepo/repository_test.go
@@ -0,0 +1,22 @@
+package domainsrepo
+
+import (
+	"go-project/models"
+	"testing"
+)
+
+func TestRepo(t *testing.T) {
+	u := models.User{Name: "Simtel", Email: "email@email.com"}
+
+	if u.Name != "Simtel" {
+		t.Errorf("Expected name to be 'Simtel', but got '%s'", u.Name)
+	}
+
+	if u.Email != "email@email.com" {
+		t.Errorf("Expected email to be 'email@email.com', but got '%s'", u.Email)
+	}
+
+	if u.GetName() != "Simtel" {
+		t.Errorf("Expected name to be 'Simtel', but got '%s'", u.GetName())
+	}
+}
--- internal/handler/http/api/main.go
+++ internal/handler/http/api/main.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
 	"go-project/common"
 	"net/http"
 )
@@ -17,10 +18,16 @@ func NewMainApi(r *chi.Mux) *MainApi {
 func (a *MainApi) AddRoutes() {
 
 	a.r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		common.SendSuccessJsonResponse(w, "Hello, World")
+		render.JSON(w, r, Home{Payload: "Hello", Success: true})
+		//common.SendSuccessJsonResponse(w, "Hello, World")
 	})
 
 	a.r.Get("/error", func(w http.ResponseWriter, r *http.Request) {
 		common.SendErrorResponse(w, "Something went wrong")
 	})
+}
+
+type Home struct {
+	Payload string `json:"payload"`
+	Success bool   `json:"success"`
 }
