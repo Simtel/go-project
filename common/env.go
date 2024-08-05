41699ab9945bac46630145371a2a4ee3f446cc33
Simtel <simuls@gmail.com>
2024-08-05 11:23:10 +0400

    fix load env

--- common/env.go
+++ common/env.go
@@ -2,10 +2,17 @@ package common
 
 import (
 	"github.com/joho/godotenv"
+	"os"
+	"regexp"
 )
 
+const projectDirName = "go-project"
+
 func InitEnv() {
-	if err := godotenv.Load(".env.local", ".env"); err != nil {
+	projectName := regexp.MustCompile(`^(.*` + projectDirName + `)`)
+	currentWorkDirectory, _ := os.Getwd()
+	rootPath := projectName.Find([]byte(currentWorkDirectory))
+	if err := godotenv.Load(string(rootPath)+`/.env.local`, string(rootPath)+`/.env`); err != nil {
 		panic("error load env file")
 	}
 
