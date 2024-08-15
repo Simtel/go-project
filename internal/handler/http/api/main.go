be3dd07aab8b28598901340f0ebd68087b1d6883
Simtel <simuls@gmail.com>
2024-08-15 10:57:12 +0400

    templates

--- internal/handler/http/api/main.go
+++ internal/handler/http/api/main.go
@@ -1,9 +1,11 @@
 package api
 
 import (
+	"bytes"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/render"
 	"go-project/internal/common"
+	"html/template"
 	"net/http"
 )
 
@@ -23,6 +25,32 @@ func (a *MainApi) AddRoutes() {
 
 	a.r.Get("/error", func(w http.ResponseWriter, r *http.Request) {
 		common.SendErrorResponse(w, "Something went wrong")
+	})
+
+	a.r.Get("/template", func(w http.ResponseWriter, r *http.Request) {
+
+		data := struct {
+			Title string
+			Name  string
+		}{
+			Title: "Пример шаблона",
+			Name:  "Мир",
+		}
+
+		tmpl, err := template.ParseFiles("internal/resource/templates/layout.html")
+		if err != nil {
+			common.SendErrorResponse(w, err.Error())
+		}
+		var buf bytes.Buffer
+
+		errExecute := tmpl.Execute(&buf, data)
+		if errExecute != nil {
+			common.SendErrorResponse(w, err.Error())
+		}
+
+		result := buf.String()
+
+		common.SendSuccessJsonResponse(w, result)
 	})
 }
 
