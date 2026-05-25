411865f0769985723b76cbf2bee1170071bba3d5
Simtel <simuls@gmail.com>
2026-05-25 14:50:11 +0400

    fix comments

--- internal/config/env.go
+++ internal/config/env.go
@@ -12,11 +12,9 @@ func InitEnv() {
 	currentWorkDirectory, _ := os.Getwd()
 	fmt.Println("Current Working Directory: " + currentWorkDirectory)
 
-	// Попробуем загрузить .env из текущей директории, затем из корня проекта
 	envLocalPath := filepath.Join(currentWorkDirectory, ".env.local")
 	envPath := filepath.Join(currentWorkDirectory, ".env")
 
-	// Если в текущей директории нет .env, ищем в корне проекта (4 уровня вверх)
 	if _, err := os.Stat(envPath); os.IsNotExist(err) {
 		rootPath := filepath.Join(currentWorkDirectory, "..", "..", "..", "..")
 		envLocalPath = filepath.Join(rootPath, ".env.local")
